f3909017cbe8017e5090ee166d60be713154fa8a
Mark Spooner <markspoonertkd+gitkraken@gmail.com>
2022-08-18 20:23:36 +0100

    add initial html file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (_, res) => res.send("hello!"));
+app.get("/", (_, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.listen(80, () => console.log("App is listening on port 80"));
