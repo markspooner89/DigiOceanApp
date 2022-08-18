7548afcf489695b7f587708a23e068834b262d16
Mark Spooner <markspoonertkd+gitkraken@gmail.com>
2022-08-18 20:18:16 +0100

    change to port 80

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (_, res) => res.send("hello!"));
 
-app.listen(3000, () => console.log("App is listening on port 3000"));
+app.listen(80, () => console.log("App is listening on port 80"));
