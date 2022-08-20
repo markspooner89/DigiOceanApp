3345eb365e663ca5a80f396c85d61dc8faef0dc3
Mark Spooner <markspoonertkd+gitkraken@gmail.com>
2022-08-20 18:04:25 +0100

    commit

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
-app.get("/", (_, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/", (_, res) => res.send("hi"));
 
 app.listen(5000, () => console.log("App is listening on port 5000"));
