a09fc34658fbf38c5c27918859ad627811899abb
Mark Spooner <markspoonertkd+gitkraken@gmail.com>
2022-08-20 18:50:18 +0100

    send file

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
+const path = require("path");
+
 const app = express();
 
-app.get("/", (_, res) => res.send("hi"));
+app.get("/", (_, res) => {
+    const filePath = path.join(__dirname, "index.html");
+    res.sendFile(filePath);
+});
 
 app.listen(5000, () => console.log("App is listening on port 5000"));
