96609d2f5dcbd549ae3aa04895634a7c1ae12c97
Mark Spooner <markspoonertkd+gitkraken@gmail.com>
2022-08-26 23:25:12 +0100

    add api call

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
-
+const favicon = require("serve-favicon");
 const path = require("path");
 
 const app = express();
 
+app.use(favicon(__dirname + "/icons8-circled-m-32.png"));
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get("/", (req, res) => {
-    console.log(req);
+app.get("/", (_, res) => {
     res.render("index");
+});
+
+app.get("/api/index", (_, res) => { 
+    res.send({
+        title: "Welcome!"
+    });
 });
 
 app.listen(5000, () => console.log("App is listening on port 5000"));
