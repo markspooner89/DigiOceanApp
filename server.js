0ad7e311fe860a43fcbac6768d024c659c91d926
Mark Spooner <markspoonertkd+gitkraken@gmail.com>
2022-09-01 21:27:12 +0100

    add some buttons and ifs

--- server.js
+++ server.js
@@ -9,9 +9,24 @@ app.use(favicon(__dirname + "/icons8-circled-m-32.png"));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get("/", (_, res) => {
+const axios = require("axios");
+
+const getPokemon = async offset => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${1}`);
+    return response.data.results;
+};
+
+app.get("/", async (req, res) => {
+    let currentIndex = 0;
+    if (req.query.offset) {
+        currentIndex = parseInt(req.query.offset);
+    }
     res.render("index", {
-        pageTitle: "Welcome!"
+        pokemon: await getPokemon(currentIndex),
+        nextIndex: currentIndex + 1,
+        previousIndex: currentIndex - 1,
+        showPreviousButton: currentIndex > 0,
+        showNextButton: currentIndex < 251
     });
 });
 
